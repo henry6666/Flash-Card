6cdc30d25bdb75820ff63b656526650afdada937
Henry Aguinaga <aguinagahenry@gmail.com>
2018-10-10 15:14:50 -0500

    Added items to layout, added a title label and a label to display card question/answer and two buttons to add and delete. Connected one outlet and three actions for the two buttons and tap gesture recognizer

--- FlashCard/ViewController.swift
+++ FlashCard/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var lblDisplayCard: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addCard(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func deleteCard(_ sender: UIButton) {
+    }
+    
+    @IBAction func tapLabel(_ sender: UITapGestureRecognizer) {
+    }
+    
 }
 
